37a1139648bba2e0f70de92d9ea1c51c97950b32
eunjin0212 <eunjin9639@gmail.com>
2023-05-28 17:41:05 +0900

    ch02-2 외부 import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import IPerson from './person/IPerson'
 import Person, { makePerson } from './person/Person'
+import Chance from 'chance'
+import * as R from 'ramda'
 
-const testMakePerson = (): void => {
-  let Kane: IPerson = makePerson('Kane')
-  let Jack: IPerson = new Person('Jack')
-  console.log(Kane, Jack)
-}
-testMakePerson()
+const chance = new Chance()
+
+let persons: IPerson[] = R.range(0, 2).map((n:number) => new Person(chance.name(), chance.age()))
+console.log(persons)
